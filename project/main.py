6749501b5445113ea1ecde02ee5476b5f8d8b15a
Gabriel Centeno <gabrielc0007@gmail.com>
2020-01-27 21:57:41 -0500

    added required check fields in signup

--- project/main.py
+++ project/main.py
@@ -11,9 +11,15 @@ from .helpers import refresh_access_token, get_access_and_refresh
 
 main = Blueprint('main', __name__)
 
+class PlaylistClass:
+	def __init__(self, too_short):
+		self.too_short = too_short
+plist = PlaylistClass(False)
+
 @main.route('/')
 def index():
     return render_template('index.html')
+
 
 @main.route('/profile')
 @login_required
@@ -38,9 +44,9 @@ def profile():
 		db.session.commit()
 
 	sp = spotipy.Spotify(auth=access_token)
-	playlist_links = PlaylistDetails.query.filter_by(user_id=user.id).all()
+	playlist_links = [playlist.playlist_uri for playlist in PlaylistDetails.query.filter_by(user_id=user.id).all()]
 	return render_template('default_profile.html', name=current_user.first_name, user_type=user.user_type,
-							playlist_links=playlist_links)
+							playlist_links=playlist_links, too_short=plist.too_short)
 
 
 @main.route('/check_playlist', methods=['POST'])
@@ -49,10 +55,10 @@ def check_playlist():
 	user = Users.query.filter_by(email=current_user.email).first()
 	uri = request.form.get('playlist_uri')
 	playlist = sp.playlist(playlist_id=uri, fields='name,followers,tracks')
-	playlist_links = [playlist['uri'] for playlist in PlaylistDetails.query.filter_by(user_id=user.id).all()]
+	playlist_links = [playlist.playlist_uri for playlist in PlaylistDetails.query.filter_by(user_id=user.id).all()]
 	if (playlist['followers']['total']) < 750:
-		return render_template('default_profile.html', name=current_user.first_name, user_type=user.user_type,
-							playlist_links=playlist_links, too_short=True)
+		plist.too_short = True
+		return redirect(url_for('main.profile'))
 
 	if PlaylistDetails.query.filter_by(playlist_uri=uri).first() is None:
 		playlist_details = PlaylistDetails(user.id, playlist['name'], uri, playlist['followers']['total'], 
@@ -60,15 +66,14 @@ def check_playlist():
 		db.session.add(playlist_details)
 		db.session.commit()
 
-	for track in playlist['tracks']['items']:
-		artist_name = track['track']['artists'][0]['name']
-		artist_in_paylist = ArtistsInPlaylist(PlaylistDetails.query.filter_by(playlist_uri=uri).first().id, 
-												artist_name=artist_name)
-		db.session.add(artist_in_paylist)
-		db.session.commit()
+		for track in playlist['tracks']['items']:
+			artist_name = track['track']['artists'][0]['name']
+			artist_in_paylist = ArtistsInPlaylist(PlaylistDetails.query.filter_by(playlist_uri=uri).first().id, 
+													artist_name=artist_name)
+			db.session.add(artist_in_paylist)
+			db.session.commit()
 	playlist_links = PlaylistDetails.query.filter_by(user_id=user.id).all()
-
-	return render_template('default_profile.html', name=current_user.first_name, user_type=user.user_type,
-							playlist_links=playlist_links, too_short=False)
+	plist.too_short = False
+	return redirect(url_for('main.profile'))
 
     
